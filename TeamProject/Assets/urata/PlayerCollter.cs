3701a479d52f95a36e50dd2117e341909275e098
urata_ayase <h1202419003@hiratagakuen.onmicrosoft.com>
2024-10-30 11:29:50 +0900

    Revert "スペースキー操作でのジャンプ、下矢印でのしゃがむ"
    This reverts commit 2ac7b2cf2dfaef5e9511d061ceecbc5a726f426e.

--- TeamProject/Assets/urata/PlayerCollter.cs
+++ TeamProject/Assets/urata/PlayerCollter.cs
@@ -4,30 +4,12 @@ using UnityEngine;
 
 public class PlayerCollter : MonoBehaviour
 {
-    //‚µ‚á‚ª‚Þ
-    private Animator anim = null;
 
-    //•à‚­
-    private float STEP = 2.5f;
+    private float STEP = 5.0f;
 
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
 
     void Update()
     {
-        float horizontalKey = Input.GetAxis("Horizontal");
-
-        if (horizontalKey < 0)
-        {
-            anim.SetBool("squat", true);
-        }
-        else
-        {
-            anim.SetBool("squat", false);
-        }
-
         //‰EŒü‚«
         this.transform.position += new Vector3(STEP * Time.deltaTime, 0, 0);
     }
